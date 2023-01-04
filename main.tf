228015d39345e54d8dc5726a41dec7a8fe308890
ericksinh0 <121467220+ericksinh0@users.noreply.github.com>
2023-01-03 18:17:46 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -124,4 +124,7 @@ resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
   }
 }
 
-
+resource "ibm_is_floating_ip" "floatingip3" {
+  name = "fip3"
+  target = "${ibm_is_instance.instance3.primary_network_interface.0.id}"
+}
